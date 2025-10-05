bec1214fc9ddd8b4161ecc9f7133b415e5361aa1
Shawez <141575345+shawez8@users.noreply.github.com>
2025-10-05 11:59:53 +0530

    Create Day8.py

---
+++ Day8.py
@@ -0,0 +1,18 @@
+
+#Typecasting!
+#the conversion of one data type into the other data type is known as typecasting in python or type conversion in python.
+#methods: int(), float(), str(), etc.
+
+
+
+
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a)+int(b))
+
+#Implicit Typecasting
+c = 1.9
+d = 8
+print(c + d)
