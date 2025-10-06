6edb6f1a705ae99184a7d1de40488d49841204d8
Shawez <141575345+shawez8@users.noreply.github.com>
2025-10-07 00:00:46 +0530

    Python Programming

---
+++ Day10.py
@@ -0,0 +1,17 @@
+name = "Shawez"
+friend = "Avi"
+anotherfriend = 'Myself'
+print("Hello ", name)
+
+apple = '''Hey harry 
+he said , "i want to eat an apple"'''
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+for character in apple:
+    print(character)
