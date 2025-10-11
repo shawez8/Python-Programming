4ef1cff07dbbddebd37f19d2cc11be58698fdff6
Shawez <141575345+shawez8@users.noreply.github.com>
2025-10-11 20:47:03 +0530

    Python Programming

---
+++ Day11.py
@@ -0,0 +1,11 @@
+names = "Shawez,AVi,Ritik"
+print(names[0:6])
+print(len(names))
+
+fruit = "mango"
+len1 = len(fruit)
+print(len1)
+
+print(fruit[0:5])
+print(fruit[0:-2])   
+print(fruit[0:-2])    
---
+++ Day12.py
@@ -0,0 +1,12 @@
+#Strings are immutable
+
+a = "Shawez !!!!! Avi"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Shawez", "AVi"))
+print(a.split(" "))
+
+blogheading = "introduction to js"
+print(blogheading.capitalize())
